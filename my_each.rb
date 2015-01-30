ac41732d4cbd17b137a38fb1a1195494a67dc689
axton21 <axton_n@yahoo.com>
2015-01-30 13:24:22 -0500

    lab

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
-end
+#while looping**
+#increnate the index.lenngth
+#yield to the num
+	def my_each(array)
+		#array = []
+		index = 0
+		while index < array.length
+			yield(array[index])
+			index += 1
+		end
+		return array
+	end	
+
+#array, yield is the key to built the method**
